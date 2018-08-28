7cecbe3f74a0d4651a866cf7fca781823087918d
Brokkonaut <hannos17@gmx.de>
2018-08-28 19:35:36 +0200

    Format sources

--- src/main/java/com/mtihc/minecraft/myhelppages/IHelpConfiguration.java
+++ src/main/java/com/mtihc/minecraft/myhelppages/IHelpConfiguration.java
@@ -11,41 +11,43 @@ import java.util.Set;
  */
 public interface IHelpConfiguration {
 
-	/**
-	 * @return the title of a page
-	 */
-	public abstract String getMessagePageTile();
+    /**
+     * @return the title of a page
+     */
+    public abstract String getMessagePageTile();
 
-	/**
-	 * @return message to be sent when the player has no permission to view the requested page
-	 */
-	public abstract String getMessageNoPagePermission();
+    /**
+     * @return message to be sent when the player has no permission to view the requested page
+     */
+    public abstract String getMessageNoPagePermission();
 
-	/**
-	 * @return message to be sent when the requested page was not found
-	 */
-	public abstract String getMessagePageNotFound();
-	
-	/**
-	 * @param name The page name
-	 * @return lines of text on the specified page
-	 */
-	public abstract List<String> getPage(String name);
+    /**
+     * @return message to be sent when the requested page was not found
+     */
+    public abstract String getMessagePageNotFound();
 
-	/**
-	 * @param name The page name
-	 * @return whether the page exists
-	 */
-	public abstract boolean hasPage(String name);
+    /**
+     * @param name
+     *            The page name
+     * @return lines of text on the specified page
+     */
+    public abstract List<String> getPage(String name);
 
-	/**
-	 * @return Set of all page names
-	 */
-	public abstract Set<String> getPageNames();
+    /**
+     * @param name
+     *            The page name
+     * @return whether the page exists
+     */
+    public abstract boolean hasPage(String name);
 
-	/**
-	 * Reloads any changes to the configuration
-	 */
-	public abstract void reload();
+    /**
+     * @return Set of all page names
+     */
+    public abstract Set<String> getPageNames();
+
+    /**
+     * Reloads any changes to the configuration
+     */
+    public abstract void reload();
 
 }
--- src/main/java/com/mtihc/minecraft/myhelppages/util/YamlFile.java
+++ src/main/java/com/mtihc/minecraft/myhelppages/util/YamlFile.java
@@ -1,6 +1,5 @@
 package com.mtihc.minecraft.myhelppages.util;
 
-
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -20,110 +19,115 @@ import org.bukkit.plugin.java.JavaPlugin;
  */
 public class YamlFile {
 
-	protected final JavaPlugin plugin;
-	protected final String name;
-	protected final File file;
-	
-	private YamlConfiguration config = null;
-	
+    protected final JavaPlugin plugin;
+    protected final String name;
+    protected final File file;
 
-	/**
-	 * Constructor
-	 * @param plugin The plugin
-	 * @param name the filename without .yml extension
-	 */
-	public YamlFile(JavaPlugin plugin, String name) {
-		if (plugin == null) {
-			throw new NullPointerException("Parameter plugin must be non-null.");
-		}
-		if (name == null) {
-			throw new NullPointerException("Parameter name must be non-null.");
-		}
-		this.plugin = plugin;
-		this.name = name;
-		
-		File dataFolder = plugin.getDataFolder();
-		file = new File(dataFolder, this.name + ".yml");
-	}
-	
-	/**
-	 * Returns the name of the file
-	 * @return The name of the file
-	 */
-	public String getName() { return name; }
+    private YamlConfiguration config = null;
 
-	/**
-	 * Returns the currently loaded configuration
-	 * @return currently loaded config
-	 */
-	public YamlConfiguration getConfig() {
-		return config;
-	}
+    /**
+     * Constructor
+     * 
+     * @param plugin
+     *            The plugin
+     * @param name
+     *            the filename without .yml extension
+     */
+    public YamlFile(JavaPlugin plugin, String name) {
+        if (plugin == null) {
+            throw new NullPointerException("Parameter plugin must be non-null.");
+        }
+        if (name == null) {
+            throw new NullPointerException("Parameter name must be non-null.");
+        }
+        this.plugin = plugin;
+        this.name = name;
 
-	/**
-	 * Loads the yml file
-	 */
-	public void reload() {
-		try {
-			config = YamlConfiguration.loadConfiguration(file);
-			setDefaults(this.name);
-		}
-		catch(Exception e) {
-			plugin.getLogger().log(Level.WARNING, plugin.getDescription().getFullName() + " could not load file: " + file + " ", e);
-		}
-	}
+        File dataFolder = plugin.getDataFolder();
+        file = new File(dataFolder, this.name + ".yml");
+    }
 
-	/**
-	 * Set default values from the plugin's resources.
-	 * Specify a fileName without extension.
-	 * @param fileName The name of the file with default values
-	 */
-	public void setDefaults(String fileName) {
-		
-		InputStream defConfigStream = plugin.getResource(fileName + ".yml");
-		if (defConfigStream != null) {
-			YamlConfiguration defConfig = YamlConfiguration
-				.loadConfiguration(new InputStreamReader(defConfigStream, Charset.forName("UTF-8")));
-			config.options().copyDefaults(true);
-			config.setDefaults(defConfig);
-			save();
-		}
-	}
-	
+    /**
+     * Returns the name of the file
+     * 
+     * @return The name of the file
+     */
+    public String getName() {
+        return name;
+    }
 
-	/**
-	 * Add default values from the plugin's resources.
-	 * Specify a fileName without extension.
-	 * @param fileName The name of the file with default values
-	 */
-	public void addDefaults(String fileName) {
-		
-		InputStream defConfigStream = plugin.getResource(fileName + ".yml");
-		if (defConfigStream != null) {
-			YamlConfiguration defConfig = YamlConfiguration
-			        .loadConfiguration(new InputStreamReader(defConfigStream, Charset.forName("UTF-8")));
-			config.options().copyDefaults(true);
-			config.addDefaults(defConfig);
-			save();
-		}
-	}
+    /**
+     * Returns the currently loaded configuration
+     * 
+     * @return currently loaded config
+     */
+    public YamlConfiguration getConfig() {
+        return config;
+    }
 
-	/**
-	 * Saves the yml file
-	 */
-	public void save() {
-		try {
-			config.save(file);
-		} catch (IOException e) {
-			plugin.getLogger().log(Level.SEVERE,
-					plugin.getDescription().getFullName() + " could not save to file: " + file + " ", e);
-		}
-	}
+    /**
+     * Loads the yml file
+     */
+    public void reload() {
+        try {
+            config = YamlConfiguration.loadConfiguration(file);
+            setDefaults(this.name);
+        } catch (Exception e) {
+            plugin.getLogger().log(Level.WARNING, plugin.getDescription().getFullName() + " could not load file: " + file + " ", e);
+        }
+    }
 
-	/**
-	 * @return the plugin
-	 */
-	public JavaPlugin getPlugin() {
-		return plugin;
-	}
+    /**
+     * Set default values from the plugin's resources.
+     * Specify a fileName without extension.
+     * 
+     * @param fileName
+     *            The name of the file with default values
+     */
+    public void setDefaults(String fileName) {
+
+        InputStream defConfigStream = plugin.getResource(fileName + ".yml");
+        if (defConfigStream != null) {
+            YamlConfiguration defConfig = YamlConfiguration.loadConfiguration(new InputStreamReader(defConfigStream, Charset.forName("UTF-8")));
+            config.options().copyDefaults(true);
+            config.setDefaults(defConfig);
+            save();
+        }
+    }
+
+    /**
+     * Add default values from the plugin's resources.
+     * Specify a fileName without extension.
+     * 
+     * @param fileName
+     *            The name of the file with default values
+     */
+    public void addDefaults(String fileName) {
+
+        InputStream defConfigStream = plugin.getResource(fileName + ".yml");
+        if (defConfigStream != null) {
+            YamlConfiguration defConfig = YamlConfiguration.loadConfiguration(new InputStreamReader(defConfigStream, Charset.forName("UTF-8")));
+            config.options().copyDefaults(true);
+            config.addDefaults(defConfig);
+            save();
+        }
+    }
+
+    /**
+     * Saves the yml file
+     */
+    public void save() {
+        try {
+            config.save(file);
+        } catch (IOException e) {
+            plugin.getLogger().log(Level.SEVERE, plugin.getDescription().getFullName() + " could not save to file: " + file + " ", e);
+        }
+    }
+
+    /**
+     * @return the plugin
+     */
+    public JavaPlugin getPlugin() {
+        return plugin;
+    }
 }
